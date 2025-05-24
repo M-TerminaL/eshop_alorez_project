6baff570b5fdec6effc9111d3a357f75ec639bf6
Alireza Babaei <andromedaa5692@gmail.com>
2025-05-24 18:40:11 +0330

    startapp home_module and config mvt for start

--- eshop_alorez_project/settings.py
+++ eshop_alorez_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Internal apps
+    'home_module',
+    # External apps
 ]
 
 MIDDLEWARE = [
